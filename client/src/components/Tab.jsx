580642c7381cbf9b99e04f06850ff8e0338e23b3
Administrator <chirateep.win@gmail.com>
2023-10-02 16:06:08 +0700

    first commit

---
+++ client/src/components/Tab.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+
+
+const Tab = ({tab,isFilterTab,isActiveTab,handleclick}) => {
+const snap = useSnapshot(state);
+
+const activeStyles = isFilterTab && isActiveTab ?
+{backgroundcolor: snap.color,opacity:0.5}
+:{backgroundcolor:'transparent',opacity:1}
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn  ${isFilterTab ? 'rounded-full glassmorhism' : 'rounded-4'  }`}
+    onClick={handleclick}
+    style={activeStyles}
+    >
+      <img
+      src={tab.icon}
+      alt={tab.name}
+      className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/2 object-contain'}`}
+      />
+    </div>
+  )
+}
+
+export default Tab
